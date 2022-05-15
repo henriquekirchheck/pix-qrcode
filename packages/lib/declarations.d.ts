0d0520460a41422652355bf4659b7c41ceef887c
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-05-15 04:05:01 -0300

    pix code is now succesfully generated

---
+++ packages/lib/declarations.d.ts
@@ -0,0 +1,1 @@
+declare module 'steplix-emv-qrcps'
--- packages/lib/src/qrcode-gen.ts
+++ packages/lib/src/qrcode-gen.ts
@@ -1,5 +1,40 @@
-const createQrCode = () => {
-  console.log("Hi")
+import { Merchant } from 'steplix-emv-qrcps'
+
+const createPixCode = (
+  key: string,
+  amount: number,
+  name: string,
+  key_type: 'telefone' | 'cnpj' | 'cpf' | 'email',
+  city: string
+) => {
+  if (key_type !== 'email') key = key.replace(/\D/g, '')
+  if (key_type === 'telefone' && !key.includes('+55')) key = '+55' + key
+
+  const emvqr = Merchant.buildEMVQR()
+  emvqr.setPayloadFormatIndicator('01')
+  emvqr.setCountryCode('BR')
+  emvqr.setMerchantCategoryCode('0000')
+  emvqr.setTransactionCurrency('986')
+
+  const merchantAccountInformation = Merchant.buildMerchantAccountInformation()
+  merchantAccountInformation.setGloballyUniqueIdentifier('BR.GOV.BCB.PIX')
+
+  merchantAccountInformation.addPaymentNetworkSpecific('01', key)
+
+  const additionalDataFieldTemplate =
+    Merchant.buildAdditionalDataFieldTemplate()
+  additionalDataFieldTemplate.setReferenceLabel('***')
+
+  emvqr.addMerchantAccountInformation('26', merchantAccountInformation)
+  emvqr.setAdditionalDataFieldTemplate(additionalDataFieldTemplate)
+
+  if (name) emvqr.setMerchantName(name)
+
+  if (city) emvqr.setMerchantCity(city)
+
+  if (amount) emvqr.setTransactionAmount(`${amount}`)
+
+  return emvqr.generatePayload()
 }
 
-export { createQrCode }
+export { createPixCode }
