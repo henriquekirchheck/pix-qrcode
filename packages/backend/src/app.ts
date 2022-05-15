4a016032eb2e54cfc1559341a216b829c6aa885d
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-05-15 15:30:19 -0300

    -Made a generate pix endpoint - Renamed localhost to 127.0.0.1

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,5 +1,44 @@
-import express from 'express'
+import { createPixCode, createQrCode } from '@pix-qrcode/lib'
+import express, { json } from 'express'
+
+interface PixRequest {
+  key: string
+  amount: number
+  name: string
+  key_type: 'telefone' | 'cnpj' | 'cpf' | 'email'
+  city: string
+  size?: {
+    width: number
+    height: number
+  }
+}
 
 const app = express()
+
+app.use(json())
+
+app.get('/api/gen-pix', (req, res) => {
+  const { size, ...data } = req.body as PixRequest
+
+  const code = createPixCode(
+    data.key,
+    data.amount,
+    data.name,
+    data.key_type,
+    data.city
+  )
+
+  createQrCode(code, size)
+    .then((qrcode) => {
+      res.status(200).json({
+        code,
+        qrcode,
+        ...data,
+      })
+    })
+    .catch((error) => {
+      res.status(502).json(error)
+    })
+})
 
 export { app }
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,7 @@
 import { app } from './app'
 
 const port = parseInt(process.env['PORT'] || '8080')
-const hostname = process.env['HOSTNAME'] || 'localhost'
+const hostname = process.env['HOSTNAME'] || '127.0.0.1'
 
 app.listen(port, hostname, () => {
   console.log(`🚀 Server running at http://${hostname}:${port}/`)
