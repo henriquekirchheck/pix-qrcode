ad013d9aaac0c97e801900404d721205cb9da0bb
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-05-15 14:25:06 -0300

    Fix ESModules

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -4,5 +4,5 @@ const port = parseInt(process.env['PORT'] || '8080')
 const hostname = process.env['HOSTNAME'] || 'localhost'
 
 app.listen(port, hostname, () => {
-  console.log('🚀 Server running at ${hostname}:${port}')
+  console.log(`🚀 Server running at http://${hostname}:${port}/`)
 })
