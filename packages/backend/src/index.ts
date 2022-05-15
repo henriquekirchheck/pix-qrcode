305d1aa2e4a3ebbf948d27199173204084cbe4c1
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-05-15 14:18:53 -0300

    Added boilerplate to backend

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,8 @@
+import { app } from './app'
+
+const port = parseInt(process.env['PORT'] || '8080')
+const hostname = process.env['HOSTNAME'] || 'localhost'
+
+app.listen(port, hostname, () => {
+  console.log('🚀 Server running at ${hostname}:${port}')
+})
